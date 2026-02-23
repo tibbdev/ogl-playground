450e1af15792bfecbb026eb527384a31e4bbb2f0
Marcus Tibbitts <mtibbitts@pm.me>
2026-02-23 01:59:55 +0000

    trying to get packed binary running to work properly

--- 02-shader-play/src/resources.cpp
+++ 02-shader-play/src/resources.cpp
@@ -3,21 +3,48 @@
 #include <physfs.h>
 #include <vector>
 #include <string>
+#include <iostream>
 
-void resources_init(const char* argv0) {
-    PHYSFS_init(argv0);
+void resources_init(const char* argv0) 
+{
+    std::cout << "Initialising Resources..." << std::endl;
+    if (!PHYSFS_init(argv0)) 
+    {
+        std::cout << "PhysFS Error: " << PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()) << std::endl;
+    }
 
-    // 1. Mount the executable itself (it will find the appended ZIP)
-    PHYSFS_mount(PHYSFS_getBaseDir(), NULL, 1);
+    std::string baseDir = PHYSFS_getBaseDir();
+    std::string exePath = baseDir + "shader-play-ogl.exe";
+
+    std::cout << "baseDir := " << baseDir << std::endl;
+    std::cout << "exePath := " << exePath << std::endl;
+
+    // PhysFS directories
+    if (!PHYSFS_mount(PHYSFS_getBaseDir(), "/", 1))
+    {
+        std::cout << "Failed to mount EXE : " << exePath << PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode());
+    }
 
     // 2. Also mount the source folders (so changes show up without rebuilding)
     // Only do this during development!
-    PHYSFS_mount("../shaders", "/shaders", 0);
-    PHYSFS_mount("../assets", "/assets", 0);
+    PHYSFS_mount("./shaders", "/shaders", 1);
+    PHYSFS_mount("./assets", "/assets", 1);
+
+    char** rc = PHYSFS_enumerateFiles("/");
+    for (char** filename = rc; *filename != NULL; filename++) {
+        std::cout << "PhysFS sees: [" << *filename << "]" << std::endl;
+    }
+    PHYSFS_freeList(rc);
+
+    std::cout << "...done." << std::endl;
 }
 
 std::string resources_load_file_to_string(const std::string& filename) {
-    if (!PHYSFS_exists(filename.c_str())) return "";
+    if (!PHYSFS_exists(filename.c_str())) 
+    {
+        std::cout << "PHYSFS File Not Found! [" << filename << "]" << std::endl;
+        return "";
+    }
 
     PHYSFS_File* file = PHYSFS_openRead(filename.c_str());
     PHYSFS_sint64 size = PHYSFS_fileLength(file);
