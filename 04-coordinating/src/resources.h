3d2e8e33cfd990a08e10db969ec8ca0f1a3f37ae
Marcus Tibbitts <mtibbitts@pm.me>
2026-02-28 10:11:50 +0000

    initial start at learnOpenGL coordinate systems

---
+++ 04-coordinating/src/resources.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdint.h>
+#include <string>
+
+struct ImageData
+{
+	uint8_t* pixels = nullptr;
+	int		 width = 0;
+	int		 height = 0;
+	int		 channels = 0;
+};
+
+void resources_init(const char* argv0);
+std::string resources_load_file_to_string(const std::string& filename);
+ImageData resources_load_image(const std::string& filename);
